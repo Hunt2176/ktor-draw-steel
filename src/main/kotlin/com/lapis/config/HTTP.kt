f73f94eee5de2d8c0e2b49b37a03f960fde0f009
Hunter Forbus <huntermitzi@gmail.com>
2025-01-19 14:00:21 -0600

    Added error json responses.

--- src/main/kotlin/com/lapis/config/HTTP.kt
+++ src/main/kotlin/com/lapis/config/HTTP.kt
@@ -1,11 +1,13 @@
 package com.lapis.config
 
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.plugins.partialcontent.*
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 fun Application.configureHTTP()
@@ -29,4 +31,28 @@ fun Application.configureHTTP()
 		// If the HTTP request specifies more ranges, they will all be merged into a single range.
 		maxRangeCount = 10
 	}
+	
+	val p = createRouteScopedPlugin("ErrorToJson") {
+		onCallRespond { call ->
+			val needsJsonResp = call.request.headers.getAll(HttpHeaders.Accept)?.contains(ContentType.Application.Json.toString()) != true
+			val isError = call.response.status()?.isSuccess() == false
+			
+			if (needsJsonResp || !isError) {
+				return@onCallRespond
+			}
+			
+			transformBody { body ->
+				if (body !is String) {
+					body
+				}
+				else {
+					val resp = mapOf("error" to body)
+					val jResp = jsonBuilder.encodeToString(resp)
+					jResp
+				}
+			}
+		}
+	}
+	
+	install(p)
 }
