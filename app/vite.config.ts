ec3a7cb453053d3a9724786b79000f704277d06a
Hunter Forbus <huntermitzi@gmail.com>
2025-02-05 19:15:51 -0600

    Add babel-plugin-react-compiler and eslint-plugin-react-compiler dependencies; update Vite configuration

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
     react({
       babel: {
         plugins: [
-          ['module:@preact/signals-react-transform']
+          ['module:@preact/signals-react-transform'],
+          ["babel-plugin-react-compiler"]
         ]
       }
     }),
